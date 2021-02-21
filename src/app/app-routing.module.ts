96c5edaa79695b14881b807341445265219c08a0
Louka Soret <louka.soret@gmail.com>
2021-02-21 11:22:55 +0100

    fix routing initialisation bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,33 +2,37 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectUnauthorizedTo(['login'])}
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'list-details/:listId',
-    loadChildren: () => import('./pages/list-details/list-details.module').then( m => m.ListDetailsPageModule),
-    canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectUnauthorizedTo(['login'])}
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
-    canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectLoggedInTo(['home'])}
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
-    canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectLoggedInTo(['home'])}
-  }
-];
+const routes: Routes = [];
+
+routes.push(  {
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+  canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectUnauthorizedTo(['login'])}
+});
+
+routes.push(   {
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+});
+
+routes.push(    {
+  path: 'login',
+  loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+  canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectLoggedInTo(['home'])}
+});
+
+routes.push(  {
+  path: 'register',
+  loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
+  canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectLoggedInTo(['home'])}
+});
+
+routes.push(   {
+  path: 'list-details/:listId',
+  loadChildren: () => import('./pages/list-details/list-details.module').then( m => m.ListDetailsPageModule),
+  canActivate: [AngularFireAuthGuard], data: {authGuardPipe: () => redirectUnauthorizedTo(['login'])}
+});
 
 @NgModule({
   imports: [
