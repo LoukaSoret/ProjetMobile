8195aad9662ae0d81ce4b031f798cbfbdd78f488
Louka Soret <louka.soret@gmail.com>
2021-02-21 18:37:07 +0100

    Google auth with gapi not found error

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -18,11 +21,11 @@ export class LoginPage implements OnInit {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
-    })
+    });
   }
 
   login(): void {
-    if(this.loginForm.valid){
+    if (this.loginForm.valid){
       this.auth.signInWithEmailAndPassword(
         this.loginForm.get('email').value, this.loginForm.get('password').value)
         .then(() => {
@@ -31,6 +34,17 @@ export class LoginPage implements OnInit {
         .catch(() => {
           console.log('Log in error');
         });
+    }
+  }
+
+  async loginWithGoogle() {
+    try {
+      const googleUser = await Plugins.GoogleAuth.signIn() as any;
+      const credential = firebase.auth.GoogleAuthProvider.credential(googleUser.authentication.idToken);
+      await this.auth.signInAndRetrieveDataWithCredential(credential);
+      this.router.navigate(['/home']);
+    } catch (err) {
+      console.log(err);
     }
   }
 
